fecb3c23c3e66d72fac257ba79d5afd4b9917b7c
Adam Sah <140002+asah@users.noreply.github.com>
2021-08-11 09:49:49 -0400

    default to night mode

--- zerver/models.py
+++ zerver/models.py
@@ -1289,7 +1289,7 @@ class UserBaseSettings(models.Model):
     COLOR_SCHEME_NIGHT = 2
     COLOR_SCHEME_LIGHT = 3
     COLOR_SCHEME_CHOICES = [COLOR_SCHEME_AUTOMATIC, COLOR_SCHEME_NIGHT, COLOR_SCHEME_LIGHT]
-    color_scheme: int = models.PositiveSmallIntegerField(default=COLOR_SCHEME_AUTOMATIC)
+    color_scheme: int = models.PositiveSmallIntegerField(default=COLOR_SCHEME_NIGHT)
 
     # UI setting controlling Zulip's behavior of demoting in the sort
     # order and graying out streams with no recent traffic.  The
